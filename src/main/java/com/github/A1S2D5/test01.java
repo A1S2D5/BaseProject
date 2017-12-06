a58764a8039bb25f0cf82cfc1e5622c303d647a7
A1S2D5 <a1s2d5tw72@gmail.com>
2017-12-06 12:04:19 +0800

    123

---
+++ src/main/java/com/github/A1S2D5/test01.java
@@ -0,0 +1,16 @@
+package com.github.A1S2D5;
+
+public class test01 {
+
+    public boolean gettype(){
+        int i = 6;
+
+        if(i%2 == 0)
+            return true;
+        else
+            return false;
+    }
+
+
+
+}
---
+++ src/test/java/com/github/A1S2D5/class01Test.java
@@ -0,0 +1,13 @@
+package com.github.A1S2D5;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class class01Test {
+    @Test
+    public void gettype() throws Exception {
+
+    }
+
+}
